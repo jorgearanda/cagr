00abf151cce0ae0f1a885d7ee39a14d9af5fa630
Jorge Aranda <jarandag@gmail.com>
2016-09-05 15:56:53 -0700

    Add optional months argument

--- cagr.py
+++ cagr.py
@@ -1,33 +1,38 @@
 from docopt import docopt
 
 
-usage = """Usage: cagr.py [-h] --initial <initial> --final <final> --years <years>
+usage = """Usage: cagr.py [-h] --initial <initial> --final <final> [--years <years> | --months <months>]
 
 -h --help    Show this
 -i <initial>, --initial <initial>  Initial value
 -f <final>, --final <final>        Final value
 -y <years>, --years <years>        Years
+-m <months>, --months <months>     Months
 """
 
 
-def cagr(initial, final, years):
+def cagr(initial, final, years, months=None):
     """
     Compound Annual Growth Rate, given an initial and a final value for an investment,
     as well as the time elapsed (in years or fractions of years)
     """
     initial = float(initial)
     final = float(final)
-    years = float(years)
+
+    if years is not None:
+        years = float(years)
+    else:
+        years = float(months) / 12.0
 
     if initial == 0:
         raise Exception('The initial value cannot be zero')
 
     if years == 0:
-        raise Exception('The number of years cannot be zero')
+        raise Exception('The time period cannot be zero')
 
     return (final / initial) ** (1.0 / years) - 1
 
 
 if __name__ == '__main__':
     args = docopt(usage, argv=None, help=True, version=None, options_first=False)
-    cagr(args['--initial'], args['--final'], args['--years'])
+    print(cagr(args['--initial'], args['--final'], args['--years'], args['--months']))
--- test/test_cagr.py
+++ test/test_cagr.py
@@ -8,6 +8,7 @@ def test_exception_on_no_initial_value():
 
 def test_exception_on_no_time_period():
     assert_raises(Exception, cagr, 1, 1, 0)
+    assert_raises(Exception, cagr, 1, 1, None, 0)
 
 
 def test_no_increase():
@@ -16,11 +17,14 @@ def test_no_increase():
 
 def test_one_year():
     assert_almost_equals(cagr(1000, 1100, 1), 0.1)
+    assert_almost_equals(cagr(1000, 1100, None, 12), 0.1)
 
 
 def test_many_years():
     assert_almost_equals(cagr(1000, 2593.7424601, 10), 0.1)
+    assert_almost_equals(cagr(1000, 2593.7424601, None, 120), 0.1)
 
 
 def test_part_of_a_year():
     assert_almost_equals(cagr(1000, 1100, 0.5), 0.21)
+    assert_almost_equals(cagr(1000, 1100, None, 6), 0.21)
